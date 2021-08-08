63727a60265cc7588dd2e8d94c87e7edf50b8e43
Brutalpsy <=>
2021-08-08 14:47:03 +0200

    added deploy script

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,12 +1,16 @@
 export default function getBaseUrl() {
-  
-const  getQueryStringParameterByName = (name, url) => {
+  // Note: Replace the production Heroku URL below with your URL.
+  return getQueryStringParameterByName("useMockApi")
+    ? "http://localhost:3001/"
+    : "https://agile-depths-43606.herokuapp.com/";
+}
+
+const getQueryStringParameterByName = (name, url) => {
   if (!url) url = window.location.href;
   name = name.replace(/[[\]]/g, "\\$&");
   var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-      results = regex.exec(url);
+    results = regex.exec(url);
   if (!results) return null;
-  if (!results[2]) return '';
+  if (!results[2]) return "";
   return decodeURIComponent(results[2].replace(/\+/g, " "));
-
-}
+};
--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,19 +1,18 @@
-import "whatwg-fetch";
 import getBaseUrl from "./baseUrl";
 
 const baseUrl = getBaseUrl();
 
-export const  getUsers = () => {
+export const getUsers = () => {
   return get("users");
-}
+};
 
 export const deleteUser = (id) => {
   return del(`users/${id}`);
-}
+};
 
 const get = (url) => {
   return fetch(baseUrl + url).then(onSuccess, onError);
-}
+};
 
 // Can't call func delete since reserved word.
 const del = (url) => {
@@ -22,12 +21,12 @@ const del = (url) => {
   });
 
   return fetch(request).then(onSuccess, onError);
-}
+};
 
 const onSuccess = (response) => {
   return response.json();
-}
+};
 
 const onError = (error) => {
   console.log(error); // eslint-disable-line no-console
-}
+};
